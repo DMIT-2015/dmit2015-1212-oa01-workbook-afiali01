4436e4ff50ee45cfc757ab25381e2ba290927369
Afi Ali <afiali01@hotmail.com>
2022-01-12 11:48:32 -0700

    end of Jan 12 class

---
+++ dmit2015-1212-oa01-demos-afiali01/src/main/java/ca/nait/dmit/domain/Circle.java
@@ -0,0 +1,28 @@
+package ca.nait.dmit.domain;
+
+public class Circle {
+    private double radius;
+
+    public double getRadius() {
+        return radius;
+    }
+
+    public void setRadius(double radius) throws Exception{
+        if(radius <= 0){
+            throw new Exception("Radius must be greater than zero.");
+        }
+        this.radius = radius;
+    }
+
+    public Circle(){
+        radius = 1;
+    }
+
+    public Circle(double radius){
+        this.radius = radius;
+    }
+
+    public double area(){
+        return Math.PI * radius * radius;
+    }
+}
