4e5f43f277b551f154d1d9571de4eaaa20deba3a
Afi Ali <afiali01@hotmail.com>
2022-02-16 11:30:54 -0700

    Added Dtos to and oracle works

--- dmit2015-1212-oa01-jpa-demo/src/test/java/dmit2015/repository/MovieRepositoryIT.java
+++ dmit2015-1212-oa01-jpa-demo/src/test/java/dmit2015/repository/MovieRepositoryIT.java
@@ -3,6 +3,7 @@ package dmit2015.repository;
 import common.config.ApplicationConfig;
 
 import dmit2015.entity.Movie;
+import jakarta.validation.ConstraintViolationException;
 import org.jboss.arquillian.container.test.api.Deployment;
 import org.jboss.arquillian.junit5.ArquillianExtension;
 import org.jboss.shrinkwrap.api.ShrinkWrap;
@@ -52,6 +53,19 @@ public class MovieRepositoryIT {
                 .addAsResource("META-INF/persistence.xml")
                 .addAsResource("META-INF/sql/import-data.sql")
                 .addAsWebInfResource(EmptyAsset.INSTANCE,"beans.xml");
+    }
+
+    @Order(6)
+    @Test
+    void shouldFailToCreate(){
+        Movie emptyMovie = new Movie();
+        Exception exception = assertThrows(ConstraintViolationException.class, () -> {
+            _movieRepository.add(emptyMovie);
+        });
+        assertTrue(exception.getMessage().contains("The Title is required."));
+//        assertTrue(exception.getMessage().contains("The Title must be a string with minimum length."));
+        assertTrue(exception.getMessage().contains("The Release Date field is required."));
+
     }
 
     @Order(2)
